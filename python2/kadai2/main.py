6a947dcc46ba8298df8d422f0fc7740a711967a6
koyama keisuke <cbkk1210362@gn.iwasaki.ac.jp>
2022-05-11 14:17:53 +0900

    kadai2 dekita

--- python2/kadai2/main.py
+++ python2/kadai2/main.py
@@ -2,22 +2,34 @@ from flask import Flask
 from flask import render_template
 from flask import request
 import traceback
+import math
 
 app = Flask(__name__)
+
 title = "課題2:素数判定"
+
+
+def is_prime(n):
+    if n == 1:
+        return "合成数"
+
+    for k in range(2, int(math.sqrt(n)) + 1):
+        if n % k == 0:
+            return "合成数"
+
+    return "素数"
+
 
 @app.route("/")
 def ask_num():
     return render_template("index.html", title=title)
 
+
 @app.route("/result", methods=["GET"])
-def is_prime():
-    n = request.args.get("value","")
+def prime():
+    n = request.args.get("value", "")
     try:
-        if int(n)%2 == 0:
-            s = "偶数"
-        else:
-            s = "素数"
+        s = is_prime(int(n))
     except:
         e = traceback.format_exc()
         return render_template("error.html", title=title, error=e)
