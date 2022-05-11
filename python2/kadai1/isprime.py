3d832440c73b0d06e9d00b7a2ff4f559591bf525
koyama keisuke <cbkk1210362@gn.iwasaki.ac.jp>
2022-05-11 13:52:39 +0900

    kadai1 dekita

---
+++ python2/kadai1/isprime.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+import traceback
+
+app = Flask(__name__)
+
+@app.route("/")
+def ask_num():
+    return render_template("index.html")
+
+@app.route("/result", methods=["GET"])
+def is_prime():
+    n = request.args.get("value","")
+    try:
+        if int(n)%2 == 0:
+            s = "偶数"
+        else:
+            s = "素数"
+    except:
+        e = traceback.format_exc()
+        return render_template("error.html", error=e)
+    return render_template("result.html", n=n, is_prime=s)
